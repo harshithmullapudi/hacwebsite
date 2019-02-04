2da3eb1b6c6fa5e1cf6ae47ecf106abe09becfc5
Progyan Bhattacharya <Progyan@hackerrank.com>
2019-02-04 20:16:48 +0530

    Add Catchphrase and Links

--- scripts/script.js
+++ scripts/script.js
@@ -4,55 +4,129 @@ const internsSectionElem = document.querySelector('.js-interns-section');
 const internNameElem = document.querySelector('.name');
 const internTypeElem = document.querySelector('.type');
 const internLinkElem = document.querySelector('.links');
+const internPhraseElem = document.querySelector('.phrase');
 
 const internDataMap = {
+
     aditya: {
-        name: 'Aditya Agarwal',
-        linkedin: 'itsaditya'
+        "name": "Aditya Agarwal",
+        "type": "SDE",
+        "links": [
+            "https://twitter.com/dev__adi",
+            "https://adi.surge.sh"
+        ],
+        "phrase": "L do you know, Gods of Death loves apples"
     },
     akshay: {
-        name: 'Akshay Kumar',
-        linkedin: 'akshay'
+        "name": "Akshay",
+        "type": "SDE",
+        "links": [
+            "https://github.com/kakshay21",
+            "https://linkedin.com/in/kakshay21"
+        ],
+        "phrase": "Error: 404 Not Found"
     },
     aaqa: {
-        name: 'Aaqa Ishqtaq',
-        linkedin: 'aaqa'
+        "name": "Aaqa Ishtyaq",
+        "type": "SDE",
+        "links": [
+            "https://github.com/aaqaishtyaq",
+            "https://linkedin.com/in/aaqaishtyaq",
+            "https://aaqai.me"
+        ],
+        "phrase": "May the FOSS be with you"
     },
     danish: {
-        name: 'Danish',
-        linkedin: 'danish'
+        "name": "Danish Prakash",
+        "type": "SDE",
+        "links": [
+            "https://github.com/danishprakash",
+            "https://linkedin.com/in/danishprakash",
+            "https://danishprakash.github.io"
+        ],
+        "phrase": "git pull --yourself-together"
     },
     ashish: {
-        name: 'Ashish',
-        linkedin: 'ashish'
+        "name": "Ashish Jha",
+        "type": "TCE",
+        "links": [
+            "https://github.com/marshal4world",
+            "https://www.linkedin.com/in/marshal4world/",
+            "https://codeforces.com/profile/marshal4world"
+        ],
+        "phrase": "I turn coffee into code. Given enough coffee I could rule the world :p"
     },
     srinath: {
-        name: 'Srinath',
-        linkedin: 'srinath'
+        "name": "Srinath",
+        "type": "TCE",
+        "links": [
+            "https://github.com/theKnack",
+            "https://linkedin.com/in/01srinath"
+        ],
+        "phrase": "Error: 404 Not Found"
     },
     harshith: {
-        name: 'Harshith',
-        linkedin: 'harshith'
+        "name": "Harshith Mullapudi",
+        "type": "SDE",
+        "links": [
+            "https://github.com/harshithmullapuci",
+            "https://eventbeep.com"
+        ],
+        "phrase": "I build team. We build [sS]*."
     },
     virali: {
-        name: 'Virali',
-        linkedin: 'virali'
+        "name": "Virali",
+        "type": "TCE",
+        "links": [
+            "https://github.com/#",
+            "https://linkedin.com/in/virali"
+        ],
+        "phrase": "Error: 404 Not Found"
     },
     aditi: {
-        name: 'Aditi',
-        linkedin: 'aditi'
+        "name": "Aditi Srinivas",
+        "type": "SDE",
+        "links": [
+            "https://github.com/aditisrinivas97",
+            "https://linkedin.com/in/aditisrinivas97"
+        ],
+        "phrase": "Why does the Docker container fail, Bruce? So that, Kubernetes can start it again."
     },
-    sourav: {
-        name: 'Sourav',
-        linkedin: 'sourav'
+    saurav: {
+        "name": "Saurav Sachidanand",
+        "type": "SDE",
+        "links": [
+            "https://github.com/saurvs",
+            "https://linkedin.com/in/saurvs"
+        ],
+        "phrase": "Tangerine trees and marmalade skies and a girl with kaleidoscope eyes."
     },
     sangam: {
-        name: 'Sangam',
-        linkedin: 'sangam'
+        "name": "Sangam Kumar",
+        "type": "SDE",
+        "links": [
+            "https://github.com/sangamcse",
+            "http://www.meetsangamcse.me"
+        ],
+        "phrase": "At the forefront of code excellence"
     },
     nimesh: {
-        name: 'Nimesh',
-        linkedin: 'Nimesh'
+        "name": "Nimeeh Jain",
+        "type": "SDE",
+        "links": [
+            "https://www.nimeshjain.com/",
+            "https://instagram.com/devnimi/"
+        ],
+        "phrase": "My name is Nimesh Jain and I'm not an engineer*"
+    },
+    progyan: {
+        "name": "Progyan",
+        "type": "SDE",
+        "links": [
+            "https://github.com/Progyan1997",
+            "https://linkedin.com/in/progyan-bhattacharya"
+        ],
+        "phrase": "Loves to Make, Dares to Break"
     }
 };
 function handleClick(event) {
@@ -60,12 +134,19 @@ function handleClick(event) {
     const internName = elem.dataset.name;
     const internData = internDataMap[internName] || {};
     internNameElem.innerHTML = `"${internData.name}"`;
-    internTypeElem.innerHTML = `"${internData.name}"`
+    internTypeElem.innerHTML = `"${internData.type}"`;
+    internPhraseElem.innerHTML = `<h3><span class='warning'>$</span> ${internData.phrase}</h3>`;
+    internLinkElem.innerHTML = `[
+        <a class="github" href="${internData.links[0]}">${internData.links[0].replace("https://github.com/", "").replace("https://twitter.com/", "")}</a>,
+        <a class="linkedin" href="${internData.links[1]}">${internData.links[1].replace("https://linkedin.com/in/", "").replace("https://instagram.com/", "")}</a>
+    ]`;
+    // const gitlink = elem.closest('a.github');
+    // gitlink.innerHTML = `${internData.links[0]}`;
+    // gitlink.setAttribute('href', internData.links[0]);
     // linkedinUrl = 'https://linkedin.com/in/' + internData.linkedin;
     // internLinkedinElem.innerHTML = linkedinUrl;
     // internLinkedinElem.href = linkedinUrl;
-    var typer = document.getElementById('typewriter');
-
+    var typer = null; //document.querySelector('.typewriter');
     typewriter = setupTypewriter(typer);
     typewriter.type();
 
@@ -74,7 +155,7 @@ function handleClick(event) {
 internsSectionElem.addEventListener('click', handleClick);
 
 
-// This is typewriter function 
+// This is typewriter function
 function setupTypewriter(t) {
     var HTML = t.innerHTML;
 
@@ -144,18 +225,21 @@ var typer = document.getElementById('npm-install');
 typewriter = setupTypewriter(typer);
 typewriter.type();
 
-// Start npm start 
+// Start npm start
 
 setTimeout(function(){
     document.getElementById('npm-install').style.display = 'none';
     document.getElementsByClassName('intern-detail')[0].style.display = 'block';
-    console.log(window.innerWidth)
+    document.getElementsByClassName('phrase')[0].style.display = 'block';
+    document.querySelector('footer').style.display = 'inline-flex';
+    // console.log(window.innerWidth)
     if(window.innerWidth <= 600)
     {
         document.getElementsByClassName('interns-section')[0].style.display = 'block';
     }
     else
     {
-         document.getElementsByClassName('interns-section')[0].style.display = 'grid';
+        document.getElementsByClassName('interns-section')[0].style.display = 'grid';
     }
-}, 14000)
+}, 14000);
+// 14000
